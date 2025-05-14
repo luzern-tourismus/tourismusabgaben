6f25e760449c0d5e51849f778e4dfd92a3a2a214
Lang Urs <urs.lang@luzern.com>
2025-05-14 11:15:47 +0200

    .

--- tmp/POSTGRES.md
+++
@@ -1,2 +0,0 @@
-sudo apt install postgresql postgresql-client -y
-sudo apt-get install php-pgsql -y
--- tmp/sql/setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS projects (
-    project_id   INTEGER PRIMARY KEY,
-    project_name TEXT    NOT NULL
-);
